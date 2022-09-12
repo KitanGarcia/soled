14ea73116727942322c691e89ce061aa5a49ba73
Kitan Garcia <kitangar@gmail.com>
2022-09-12 14:53:15 -0700

    logic to create simple course

---
+++ programs/soled/src/components/course.rs
@@ -0,0 +1,83 @@
+use crate::*;
+
+//
+// Endpoints
+//
+pub fn create_course(
+    ctx: Context<CreateCourse>,
+    title: String,
+    description: String,
+    thumbnail_url: String,
+) -> Result<()> {
+    let course: &mut Account<Course> = &mut ctx.accounts.course;
+    let authority: &Signer = &ctx.accounts.authority;
+    msg!("username: {}", username);
+    msg!("email: {}", email);
+    msg!("description: {}", description);
+
+    course.authority = *authority.key;
+    course.title = title;
+    course.description = description;
+    course.thumbnail_url = thumbnail_url;
+
+    Ok(())
+}
+
+
+// 
+// Data Validators
+// 
+#[derive(Accounts)]
+#[instruction(title: String, description: String, thumbnail_url: String)]
+pub struct CreateCourse<'info> {
+    // Create account of type Course and assign creator's pubkey as the payer
+    // This also makes sure that we have only one course for the following combination
+    #[account(
+        init, 
+        payer = authority, 
+        space = Course::LEN
+    )]
+    pub course: Account<'info, Course>,
+
+    // Define user as mutable - money in their account, description
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    // Ensure System Program is the official one from Solana and handle errors
+    // Ensure benefit_number == num_benefits + 1
+    pub system_program: Program<'info, System>,
+}
+
+
+
+//
+// Data Structure
+//
+#[account]
+pub struct Course {
+    pub authority: Pubkey,
+    pub title: String,
+    pub description: String,
+    pub thumbnail_url: String,
+}
+
+
+// Constants for sizing properties
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBKEY_LENGTH: usize = 32;
+const STRING_LENGTH_PREFIX: usize = 4;
+const AUTHORITY_LENGTH: usize = 8;
+const TITLE_LENGTH: usize = 20 * 4;
+const DESCRIPTION_LENGTH: usize = 200 * 4;
+const THUMBAIL_URL_LENGTH: usize = 300 * 4;
+
+impl Course {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBKEY_LENGTH
+        + STRING_LENGTH_PREFIX 
+        + TITLE_LENGTH 
+        + STRING_LENGTH_PREFIX 
+        + DESCRIPTION_LENGTH 
+        + STRING_LENGTH_PREFIX
+        + THUMBNAIL_URL_LENGTH
+}
--- programs/soled/src/lib.rs
+++ programs/soled/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
 
+pub mod components;
+use components::*;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
@@ -8,6 +11,15 @@ pub mod soled {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
+    }
+
+    pub fn create_course(
+        ctx: Context<CreateCourse>,
+        title: String,
+        description: String,
+        thumbnail_url: String,
+    ) -> Result<()> {
+        components::create_course(ctx, title, description, thumbnail_url)
     }
 }
 
