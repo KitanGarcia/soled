405e26f1f681d8e661b763a37adae462e1dfb178
Kitan Garcia <kitangar@gmail.com>
2022-11-16 19:01:26 -0800

    add connection to test utils

---
+++ tests/utils/Connection.ts
@@ -0,0 +1,17 @@
+import * as anchor from "@project-serum/anchor";
+import { ConfirmOptions } from "@solana/web3.js";
+
+export const PROGRAM_ID = new anchor.web3.PublicKey(
+  "Hen1HiTHovdX68H4PVBPTGfww6FGL3zgXTeboBZB7EwW"
+);
+
+export const OPTS = {
+  preflightCommitment: "processed",
+} as ConfirmOptions;
+
+const endpoint = "https://api.devnet.solana.com";
+
+export const connection = new anchor.web3.Connection(
+  endpoint,
+  OPTS.preflightCommitment
+);
