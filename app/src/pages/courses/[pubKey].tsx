09ce0b610b9bd1ff0bf868b2ee9b4d136fb0e2b4
Kitan Garcia <kitangar@gmail.com>
2023-03-11 15:30:12 -0800

    render course fields on coursepage and allow routing to instructor

--- app/src/pages/courses/[pubKey].tsx
+++ app/src/pages/courses/[pubKey].tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import * as anchor from '@project-serum/anchor';
 import { useAnchorWallet } from '@solana/wallet-adapter-react';
 import { AnchorProvider } from '@project-serum/anchor';
+import { PublicKey } from '@solana/web3.js';
 
 import styles from '../../styles/Home.module.css';
 import NavBar from '../../components/layout/NavBar';
@@ -12,7 +13,7 @@ import Footer from '../../components/layout/Footer';
 
 import { connection, OPTS, PROGRAM_ID } from '../../utils/Connection';
 import IDL from '../../utils/soled.json';
-import { PublicKey } from '@solana/web3.js';
+import { shortenAddress } from '../../utils/utils';
 import { Course } from '../../types/Course';
 
 const MyCourses: NextPage = () => {
@@ -38,6 +39,7 @@ const MyCourses: NextPage = () => {
       getCourse();
     }
   }, [program, router]);
+  console.log(course);
 
   return (
     <div className="bg-bg-color">
@@ -48,12 +50,31 @@ const MyCourses: NextPage = () => {
             <div className="pl-8 flex flex-row justify-between items-center">
               <img
                 className="rounded-full h-[100px] w-[100px]"
-                src="https://imgs.search.brave.com/1jotBYRliFn8D84WwWMIY7RMyracWBEeu9yv5olbKHo/rs:fit:498:498:1/g:ce/aHR0cHM6Ly9jcnlw/dG9yYW5rLWltYWdl/cy5zMy5ldS1jZW50/cmFsLTEuYW1hem9u/YXdzLmNvbS9jb2lu/cy9zb2xhbmExNjA2/OTc5MDkzMDU2LnBu/Zw"
+                src={
+                  course
+                    ? course.thumbnailUrl
+                    : 'https://imgs.search.brave.com/1jotBYRliFn8D84WwWMIY7RMyracWBEeu9yv5olbKHo/rs:fit:498:498:1/g:ce/aHR0cHM6Ly9jcnlw/dG9yYW5rLWltYWdl/cy5zMy5ldS1jZW50/cmFsLTEuYW1hem9u/YXdzLmNvbS9jb2lu/cy9zb2xhbmExNjA2/OTc5MDkzMDU2LnBu/Zw'
+                }
                 alt="Course Image"
               />
-              <h1 className="pl-6 h-9 text-2xl text-main-text tracking-widest">
-                {'SOLANA TESTNET'.toUpperCase()}
-              </h1>
+              <div className="flex flex-col items-center">
+                <h1 className="pl-6 h-9 text-2xl text-main-text tracking-widest">
+                  {(course ? `${course.title}` : 'course title').toUpperCase()}
+                </h1>
+                {course && course.title && (
+                  <p>
+                    Author:&nbsp;
+                    <span
+                      className="cursor-pointer text-transparent font-normal bg-clip-text bg-gradient-to-br from-solana-start to-solana-end"
+                      onClick={() =>
+                        router.push(`/instructors/${course.authority}`)
+                      }
+                    >
+                      {shortenAddress(course.authority)}
+                    </span>
+                  </p>
+                )}
+              </div>
             </div>
 
             <div className="my-4 mr-4 flex flex-col justify-between items-center bg-[#A0B3F9] text-main-text rounded-xl">
@@ -89,7 +110,7 @@ const MyCourses: NextPage = () => {
             <div className="flex flex-col basis-3/4 h-96 pl-24 py-16 bg-fg-color rounded-xl ml-6">
               <div className="flex justify-between items-center h-14 w-full">
                 <h1 className="font-bold text-main-text tracking-widest leading-6 text-2xl">
-                  {`SOLANA TEST`.toUpperCase()}
+                  {(course ? `${course.title}` : 'course title').toUpperCase()}
                 </h1>
                 <div className="w-1/4 flex justify-center">
                   <button className="btn h-[45px] w-[45px] rounded-full border-2 border-card-border-color-start bg-fg-color hover:bg-gradient-to-br from-solana-start to-solana-end hover:border-2 hover:border-card-border-color-start">
@@ -110,9 +131,7 @@ const MyCourses: NextPage = () => {
                   <p>Placeholder2: text</p>
                 </div>
                 <p className="pt-6 pr-6">
-                  {
-                    "Description. This will be a really long course description. It's gonna talk about how dope this course is and how you should take it. Seriously, though, you'll learn so much freaking material in this course, it won't even be funny, dude. Like oh my gawwwddd... You'll be a certified genius. Come take this course, please. Your SOL will sustain me to make other great courses such as this one to fuel both the adoption of web3 and my wallet for eons to come!!!"
-                  }
+                  {course ? course.rating : 'course description'}
                 </p>
                 <hr className="my-10 text-hr-color" />
               </div>
